5c9489781cb383cd8f9f21be782773c13818aac3
unknown <komalbatool4@gmail.com>
2022-09-19 22:29:59 +0200

    Return all telemetry data for analysis in CSV

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ def index():
 def record_movement():
     data = request.get_json(silent=True)
     if set(['speed','heading','time']).issubset(data):
+        print('--------------------', data)
 
         filtered_data = dict((k, data[k]) for k in ('speed', 'heading', 'time'))
         vehicle.record_movement(filtered_data)
@@ -24,7 +25,7 @@ def record_movement():
         return "Received invalid movement data. Make sure the data contains 'speed', 'heading' and 'time' "
     return(data)
 
-app.route('/kpis', methods=['GET'])
+@app.route('/kpis', methods=['GET'])
 def get_estimat():
     results = {
         'average_speed':vehicle.average_speed,
@@ -32,6 +33,21 @@ def get_estimat():
         'distance_from_start':vehicle.distance_from_start
     }
     return results
+
+@app.route('/download', methods=['GET'])
+def download():
+    data = vehicle.get_movements_data()
+    df = pd.DataFrame(data)
+    return Response(
+       df.to_csv(),
+       mimetype="text/csv",
+       headers={"Content-disposition":
+       "attachment; filename=telemetry_data.csv"})
+
+@app.route('/clear', methods=['GET'])
+def _clear():
+    global vehicle
+    vehicle = Vehicle()
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
